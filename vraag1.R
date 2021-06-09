ac09f663d634c32c99e2f41a4b2f14c46a42c228
RobMandelings <rob@minetronic.com>
2021-06-09 21:11:14 +0200

    Working on project

--- vraag1.R
+++ vraag1.R
@@ -3,14 +3,38 @@
 # Created by: RobMa
 # Created on: 26/05/2021
 
+library(ggplot)
+library(zoo)
+
+dataset <- read.csv('drugs_2021.csv', sep = ';')
+
 # Vraag 1: verdeling van de variabele age
 
-age <- dataset$age
+age <- (dataset$age)
+age <- na.omit(age)
+los <- dataset$los
+time <- dataset$time
 
 qqnorm(age, main = title('Normal Q-Q Plot variabele age'))
 qqline(age)
 
+age_hist <- hist(age, breaks = 30)
+xfit <- seq(min(age), max(age), length = 40) 
+yfit <- dnorm(xfit, mean = mean(age), sd = sd(age)) 
+yfit <- yfit * diff(age_hist$mids[1:2]) * length(age)
+
+lines(xfit, yfit, col = "black", lwd = 2)
+
+
+
+table(age)
+
 shapiro.test(age)
+
+# Vraag 1: normalizering
+
+hist(log(age))
+qqnorm(log(age))
 
 # Vraag 2: Hangt de efficientie af van het type programma
 
@@ -31,6 +55,12 @@ median(treat)
 table(dataset$treat, dataset$ivhx)
 chisq.test(dataset$treat, dataset$ivhx)
 
+# Vraag 4: Duur van de behandeling invloed op tijd tot herval?
+
+plot(dataset$los, dataset$time, main = "Scatterplot", xlab = "los (duur behandeling)", ylab = 'time (tijd tot herval)')
+abline(lm(dataset$los ~ dataset$time, data = mtcars), col = "red")
+
+cor.test(dataset$los, dataset$time, method = "spearman")
 #
 # quantile(age, na.rm = TRUE)
 # hist(age)
